b71ade8d3f590b59d77713954fc071d8641aa435
conwys <drkheartt@gmail.com>
2022-11-22 16:01:32 +0000

    started styling

--- train-ees/src/components/discoverPage/index.js
+++ train-ees/src/components/discoverPage/index.js
@@ -3,6 +3,7 @@ import './index.css'
 import Button from '../button'
 import DiscoverCard from '../discoverCard';
 import {useState} from 'react'
+import backgroundGif from '../images/purple_BG.gif'
 
 export default function DiscoverPage({handleClick}){
     const [cards, setCards] = useState([{}]);
@@ -26,19 +27,32 @@ export default function DiscoverPage({handleClick}){
     
     return(
         <div className = "discoverPage">
-            <div className='discoverTitle'>
-                <h1>Discover Cards</h1>
-            </div>
             <div className="discoverBody">
-                <h2>Network with your peers!</h2>
-                <p>Create a card so fellow bootcampers can find you.</p>
-                <p>Search for a bootcamper and connect with them.</p>
+                <div className='discoverTitle'>
+                    <h1>Discover Cards</h1>
+                </div>
+                <div className='discoverTitle2'>
+                    <h2>Network with your peers!</h2>
+                </div>
+                <div className='discoverBodyText'>
+                    <p>Create a card so fellow bootcampers can find you.<br/>
+                    Search for a bootcamper and connect with them.</p>
+                </div>
+                <div className='searchInput'>
+                    <input type='text' placeholder="Search By Name" onKeyDown={handleKeyDown}></input>
+                </div>
             </div>
-            <Button handleClick={handleClick} buttonText={'+ New Card'}/>
-            <div className='searchInput'>
-                <input type='text' placeholder="Search By Name" onKeyDown={handleKeyDown}></input>
+            <div className='cardDisplay'>
+                <div className='createBtn'>
+                    <Button handleClick={handleClick} buttonText={'+ New Card'}/>
+                </div>
+                <div className='discoverCard'>
+                    {cards.map((card, i) => (<DiscoverCard key={i} info={card}/>))}
+                </div>
             </div>
-            {cards.map((card, i) => (<DiscoverCard key={i} info={card}/>))}
+            <div className='purpleSection'>
+                <img src={backgroundGif} alt="background"/>
+            </div>
         </div>
     )
 }
